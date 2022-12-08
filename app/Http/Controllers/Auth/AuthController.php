578d6b155540c1b6c68e60f34003d734524d1b89
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2022-12-07 21:46:41 -0300

    cambios

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,24 +6,35 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Exception;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $data = $request->validate([
-            'email' => 'email|required',
-            'password' => 'required'
-        ]);
-
-        if (auth()->attempt($data)) {
-            $token = auth()->user()->createToken('API Token')->accessToken;
-            return response([
-                'user' => auth()->user(),
-                'token' => $token
+        try {
+            $data = $request->validate([
+                'email' => 'email|required',
+                'password' => 'required'
             ]);
-        } else {
-            return response(['error_message' => 'Usuario y/o constraseña invalido. Por favor, intente nuevamente.'], 401);
+
+            $user = User::where("email", $request->email);
+            if ($user) {
+                if (auth()->attempt($data)) {
+                    $token = auth()->user()->createToken('API Token')->accessToken;
+                    return response()->json([
+                        "success"=>true,
+                        'user' => auth()->user(),
+                        "token"=>$token,
+                    ]);
+                } else {
+                    return response(["success"=>false,'message' => 'Usuario y/o constraseña invalido. Por favor, intente nuevamente.'], 401);
+                }
+            }
+            return response()->json(["success" => false, "message" => "El usuario no se ha encontrado"]);
+
+        } catch (Exception $e) {
+            return response()->json(["success" => false, "message" => $e->getMessage()], 500);
         }
     }
 
@@ -57,7 +68,12 @@ class AuthController extends Controller
 
     public function user(Request $request)
     {
-        return response()->json($request->user());
+        try {
+            return response()->json(["success"=>true,"user"=>$request->user()]);
+        } catch (Exception $e) {
+            return response()->json(["success" => false, "message" => $e->getMessage()], 500);
+        }
+
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,11 @@ use App\Http\Controllers\Auth\AuthController;
 
 
 Route::group(['prefix' => 'auth'], function () {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
     Route::group(['middleware' => 'auth:api'], function () {
-        Route::get('/logout', [AuthController::class, 'logout']);
-        Route::get('/user', [AuthController::class, 'user']);
+        Route::get('logout', [AuthController::class, 'logout']);
+        Route::get('user', [AuthController::class, 'user']);
     });
 });
 
